326d9ad3ae565c1611416b6189318b3275803257
Subhan <subhankashif696@gmail.com>
2025-12-17 20:59:51 +0500

    Update CI/CD pipeline

--- infra/provider.tf
+++ infra/provider.tf
@@ -5,6 +5,11 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket = "ssis-tfstate-subhan-devops"
+    key    = "state/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
